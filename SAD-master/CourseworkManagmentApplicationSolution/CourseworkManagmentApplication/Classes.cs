0c6206db7b34b67a72dbd05c337a4319d91e5daf
bkvvanessa <58041447+bkvvanessa@users.noreply.github.com>
2019-11-25 22:18:02 +0000

    Add files via upload
    Added empty user classes.

---
+++ SAD-master/CourseworkManagmentApplicationSolution/CourseworkManagmentApplication/Classes.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace CourseworkManagmentApplication
+{
+    class User
+    {
+        string name;
+
+        public User()
+        {
+        }
+        User(string pName)
+        {
+            name = pName;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+    }
+
+    class DirectorOfStudy : User
+    {
+        DirectorOfStudy()
+        {
+            
+        }
+    }
+
+    class ProgrammeDirector : User
+    {
+        ProgrammeDirector()
+        {
+
+        }
+    }
+    class ModuleLeader : User
+    {
+        ModuleLeader()
+        {
+
+        }
+    }
+
+    class Academic : User
+    {
+        Academic()
+        {
+
+        }
+    }
+    class FacultyHub : User
+    {
+        FacultyHub()
+        {
+
+        }
+    }
+
+
+
+
+}
