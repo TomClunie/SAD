0a37ad4d52aaf61d389625c42dd580b0a8c1b6c0
Callum666 <integer@gmail.com>
2019-11-27 05:00:10 +0000

    Added a partial login system - more work needed

--- CourseworkManagmentApplicationSolution/CourseworkManagmentApplication/Form1.Designer.cs
+++ CourseworkManagmentApplicationSolution/CourseworkManagmentApplication/Form1.Designer.cs
@@ -28,27 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.usernameBox = new System.Windows.Forms.TextBox();
+            this.passwordBox = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // textBox1
+            // usernameBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(50, 83);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(203, 20);
-            this.textBox1.TabIndex = 0;
+            this.usernameBox.Location = new System.Drawing.Point(50, 83);
+            this.usernameBox.Name = "usernameBox";
+            this.usernameBox.Size = new System.Drawing.Size(203, 20);
+            this.usernameBox.TabIndex = 0;
             // 
-            // textBox2
+            // passwordBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(50, 138);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(203, 20);
-            this.textBox2.TabIndex = 1;
+            this.passwordBox.Location = new System.Drawing.Point(50, 138);
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.Size = new System.Drawing.Size(203, 20);
+            this.passwordBox.TabIndex = 1;
+            this.passwordBox.UseSystemPasswordChar = true;
             // 
             // button1
             // 
@@ -58,6 +59,7 @@
             this.button1.TabIndex = 2;
             this.button1.Text = "Login";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -95,11 +97,12 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.passwordBox);
+            this.Controls.Add(this.usernameBox);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -107,8 +110,8 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox usernameBox;
+        private System.Windows.Forms.TextBox passwordBox;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
--- CourseworkManagmentApplicationSolution/CourseworkManagmentApplication/Form1.cs
+++ CourseworkManagmentApplicationSolution/CourseworkManagmentApplication/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,50 @@ namespace CourseworkManagmentApplication
         public Form1()
         {
             InitializeComponent();
+        }
+        // Creating arrays with some preset values.
+        string[] username = { "Callum", "Tom" };
+        string[] password = { "admin", "loser" };
+
+        // Converting the usernames and passwords into lists of strings.
+        List<string> users = new List<string>();
+        List<string> pass = new List<string>();
+        List<string> type = new List<string>();
+        private void button1_Click(object sender, EventArgs e) // Login Button
+        {
+            // If the username & password entered in the textboxes matches the values in the array, this statement is ran.
+            if (username.Contains(usernameBox.Text) && password.Contains(passwordBox.Text) && Array.IndexOf(username, usernameBox.Text) == Array.IndexOf(password, passwordBox.Text))
+            {
+                Main mainForm = new Main(); // Creating an instance of the second window.
+                mainForm.ShowDialog(); // Opening the main application page.
+                this.Visible = false; // Closing the login page.
+
+            }
+            // Converting the users & pass values into arrays
+            else if (users.Contains(usernameBox.Text) && pass.Contains(passwordBox.Text) && Array.IndexOf(users.ToArray(), usernameBox.Text) == Array.IndexOf(pass.ToArray(), passwordBox.Text))
+            {
+                Main mainForm = new Main(); // Creating an instance of the second window.
+                mainForm.ShowDialog(); // Opening the main application page.
+                this.Visible = false; // Closing the login page.
+            }
+            else
+                // Error message
+                MessageBox.Show("The credentials you entered are invalid");
+        }
+
+        private void Form1_Load(object sender, EventArgs e) // On original form load.
+        {
+            StreamReader sr = new StreamReader("users.txt"); // The data file name and extension.
+            string line = null;
+            // While the line isn't blank.
+            while ((line = sr.ReadLine()) != null) {
+                // Seperating the usernames and passwords that are saved on the same line.
+                string[] components = line.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                users.Add(components[0]); // Adding the first component of the line to the list of usernames.
+                pass.Add(components[1]); // Adding the second component of the line to the list of passwords.
+                type.Add(components[2]); // Adding the third component of the line to the list of user types.
+            }
+            sr.Close(); // End StreamReader
         }
     }
 }
