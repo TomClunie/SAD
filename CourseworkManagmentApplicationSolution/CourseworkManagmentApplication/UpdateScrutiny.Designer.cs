a4b50c9f5bd6c29e7160e8a460b820b06bf9d149
TomClunie <gyroscope@gmail.com>
2019-12-02 16:09:39 +0000

    Updating Master
    Adding back some stuff

--- CourseworkManagmentApplicationSolution/CourseworkManagmentApplication/UpdateScrutiny.Designer.cs
+++ CourseworkManagmentApplicationSolution/CourseworkManagmentApplication/UpdateScrutiny.Designer.cs
@@ -29,57 +29,22 @@
         private void InitializeComponent()
         {
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // checkedListBox1
             // 
             this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Location = new System.Drawing.Point(24, 25);
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 12);
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(364, 334);
+            this.checkedListBox1.Size = new System.Drawing.Size(918, 409);
             this.checkedListBox1.TabIndex = 0;
             this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(25, 366);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(363, 117);
-            this.richTextBox1.TabIndex = 1;
-            this.richTextBox1.Text = "Additional Comments";
-            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(127, 546);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Submit";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(208, 546);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Clear";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // UpdateScrutiny
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(415, 603);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.richTextBox1);
+            this.ClientSize = new System.Drawing.Size(942, 446);
             this.Controls.Add(this.checkedListBox1);
             this.Name = "UpdateScrutiny";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -92,8 +57,5 @@
         #endregion
 
         private System.Windows.Forms.CheckedListBox checkedListBox1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
     }
 }
--- CourseworkManagmentApplicationSolution/CourseworkManagmentApplication/UpdateScrutiny.cs
+++ CourseworkManagmentApplicationSolution/CourseworkManagmentApplication/UpdateScrutiny.cs
@@ -17,22 +17,7 @@ namespace CourseworkManagmentApplication
         {
             InitializeComponent();
         }
-        string currentUser = Form1.cUser;
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void richTextBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
+        string currentUser = Form1.cUser;        
 
         private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -51,7 +36,7 @@ namespace CourseworkManagmentApplication
             while ((line = reader.ReadLine()) != null) //loop through each line in users.txt
             {
                 //Module Leader Checklist
-                if (line.Contains(currentUser) && line.Contains("ModLead")) //If that line contains the current username & is a module leader type
+                if (line.Contains(currentUser) && line.Contains("Module Leader")) //If that line contains the current username & is a module leader type
                 {
                     //add each line from ModuleLeaderChecklist.txt to the check box
                     StreamReader moduleLeaderReader = new StreamReader(@".\\ModuleLeaderChecklist.txt");
