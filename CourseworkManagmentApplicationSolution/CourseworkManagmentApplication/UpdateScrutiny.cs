0d1f2c605c1ba3a474f6aa3e559b6e35af7b504d
TomClunie <gyroscope@gmail.com>
2019-12-06 04:43:18 +0000

    Final

--- CourseworkManagmentApplicationSolution/CourseworkManagmentApplication/UpdateScrutiny.cs
+++ CourseworkManagmentApplicationSolution/CourseworkManagmentApplication/UpdateScrutiny.cs
@@ -16,48 +16,18 @@ namespace CourseworkManagmentApplication
         public UpdateScrutiny()
         {
             InitializeComponent();
-            for (int i = 0; i < DeadlineClasses.assignments.Count; i++)
-            {
-                comboBoxAssignment.Items.Add(DeadlineClasses.assignments[i].getName());
-            }            
+                     
         }
-        string currentUser = Form1.cUser;
         
         private void UpdateScrutiny_Load(object sender, EventArgs e)
         {
-            StreamReader reader = new StreamReader(@".\\users.txt");
 
-            string line;
-            string ModeratorLine;
             string ModLeadLine;
-            string directorOfStudyLine;
-            string externalExaminerLine;
-            while ((line = reader.ReadLine()) != null)
+            //Module Leader
+            StreamReader ModLeadReader = new StreamReader(@".\\ExternalExaminer.txt");
+            while ((ModLeadLine = ModLeadReader.ReadLine()) != null)
             {
-                //Module Leader
-                StreamReader ModLeadReader = new StreamReader(@".\\ModuleLeaderChecklist.txt");
-                while ((ModLeadLine = ModLeadReader.ReadLine()) != null)
-                {
-                    checkedListBoxModerator.Items.Add(ModLeadLine);
-                }
-                // Director of Study, Programme Director - Panel
-                StreamReader DosReader = new StreamReader(@".\\PanelChecklist.txt");
-                while ((directorOfStudyLine = DosReader.ReadLine()) != null)
-                {
-                    checkedListBoxModerator.Items.Add(directorOfStudyLine);
-                }
-                //Moderator
-                StreamReader ModReader = new StreamReader(@".\\ModeratorChecklist.txt");
-                while ((ModeratorLine = ModReader.ReadLine()) != null)
-                {
-                    checkedListBoxModerator.Items.Add(ModeratorLine);
-                }
-                //External Examiner
-                StreamReader extExamReader = new StreamReader(@".\\ExternalExaminer.txt");
-                while ((externalExaminerLine = extExamReader.ReadLine()) != null)
-                {
-                    checkedListBoxModerator.Items.Add(externalExaminerLine);
-                }
+                checkedListBoxModerator.Items.Add(ModLeadLine);
             }
         }
 
